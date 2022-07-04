658d11422a0486196281d52361115125801a43a7
Luca Somaschini <somas99@gmail.com>
2022-07-04 15:37:19 +0200

    Added two decimal places limit to final price

--- js/main.js
+++ js/main.js
@@ -9,13 +9,16 @@ let prezzoBiglietto;
 // calcolo prezzo
 if (etaPasseggero >= 18 && etaPasseggero < 65) {
     prezzoBiglietto = kmPercorso * prezzoAlKm;
-    alert('Il prezzo del biglietto è ' + prezzoBiglietto + ' (nessuno sconto applicato)');
+    let prezzoBigliettoDueDecimali = prezzoBiglietto.toFixed(2); // limito i decimali a due nel prezzo finale
+    alert('Il prezzo del biglietto è ' + prezzoBigliettoDueDecimali + ' € (nessuno sconto applicato)');
 } else if (etaPasseggero >= 65) {
     prezzoBiglietto = kmPercorso * prezzoAlKm * scontoSenior;
-    alert('Il prezzo del biglietto è ' + prezzoBiglietto + ' (è stato applicato lo sconto over 65)');
+    let prezzoBigliettoDueDecimali = prezzoBiglietto.toFixed(2); // limito i decimali a due nel prezzo finale
+    alert('Il prezzo del biglietto è ' + prezzoBigliettoDueDecimali + ' € (è stato applicato lo sconto over 65)');
 } else {
     prezzoBiglietto = kmPercorso * prezzoAlKm * scontoMinorenne;
-    alert('Il prezzo del biglietto è ' + prezzoBiglietto + ' (è stato applicato lo sconto minorenni)');
+    let prezzoBigliettoDueDecimali = prezzoBiglietto.toFixed(2); // limito i decimali a due nel prezzo finale
+    alert('Il prezzo del biglietto è ' + prezzoBigliettoDueDecimali + ' € (è stato applicato lo sconto minorenni)');
 }
 
 // debug
